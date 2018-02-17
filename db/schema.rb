061b5a18fcb90008c9c2b453cfe2c498f919c642
MermaidOnAcid <justineelizabeth@hotmail.com>
2018-02-17 16:15:26 +0100

    surname and nickname added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180217144520) do
+ActiveRecord::Schema.define(version: 20180217151306) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
@@ -42,6 +42,8 @@ ActiveRecord::Schema.define(version: 20180217144520) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "surname"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
